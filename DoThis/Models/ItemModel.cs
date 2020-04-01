a1bb13f6eab8ec948a4d16b425db77acbcad9c44
Peter Milovcik <peter.milovcik@gmail.com>
2020-04-01 12:02:13 +0200

    ListView to TreeView

--- DoThis/Models/ItemModel.cs
+++ DoThis/Models/ItemModel.cs
@@ -7,6 +7,8 @@ namespace Beeffective.Models
     {
         public int Id { get; set; }
 
+        //public int ParentId { get; set; }
+
         public string Title { get; set; }
 
         public string Categories { get; set; }
